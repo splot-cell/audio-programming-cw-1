8d0e6b6b83f4df17a760a24bd7892718d275796a
AndreasMuncier <olly_s@hotmail.co.uk>
2017-10-17 11:21:46 +0100

    Working printSamples function

---
+++ MidiOsc/MidiOsc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MidiOsc
+//
+//  Created by Olly Seber on 17/10/2017.
+//  Copyright © 2017 Olly Seber. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
